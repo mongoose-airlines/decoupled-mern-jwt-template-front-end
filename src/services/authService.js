70849ca255559d0a82ff6171aa02b26b1209112b
David <49245298+DavidStinson@users.noreply.github.com>
2020-09-14 22:23:30 -0500

    add signup service functionality

---
+++ src/services/authService.js
@@ -0,0 +1,23 @@
+const BASE_URL = '/api/auth/';
+
+function signup(user) {
+  return fetch(BASE_URL + 'signup', {
+    method: 'POST',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    body: JSON.stringify(user)
+  })
+  .then(res => {
+    console.log(res, '<-- response object')
+    return res.json();
+  })
+  .then(json => {
+    if(json.token) return json;
+    console.log(json, '<-- the error')
+    throw new Error(`${json.err}`)
+  })
+  .then(data => data)
+}
+
+export default {
+  signup
+};
