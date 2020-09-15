8c21d93b04a36d2ef92a3d5cfc84f95bc215a796
DavidStinson <mail@davidstinson.dev>
2020-09-14 21:36:37 -0500

    first commit

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <>
+      </>
+    );
+  }
+}
+ 
+export default App;
