7881dfdbe4ce95ba4599a4d4f7d41f50f3bc7ce3
David <49245298+DavidStinson@users.noreply.github.com>
2020-09-14 22:18:38 -0500

    fix app import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import App from './App';
+import App from './pages/App/App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
